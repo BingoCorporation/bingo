d5bbd8691520bbb3dd9f56038a18390387aa4802
Ebill <gjebillson@gmai.com>
2017-08-18 12:41:06 -0400

    worked on all the directories

---
+++ app/src/main/java/com/bingo/bingo/activities/SplashScreenActivity.java
@@ -0,0 +1,15 @@
+package com.bingo.bingo.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.bingo.bingo.R;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+    }
+}
