f662121c9d912e3031532125dac7e4b08256b1e4
Ebill <gjebillson@gmai.com>
2017-08-20 18:34:59 -0400

    Pushing

--- app/src/main/java/com/bingo/bingo/activities/SignInActivity.java
+++ app/src/main/java/com/bingo/bingo/activities/SignInActivity.java
@@ -3,36 +3,43 @@ package com.bingo.bingo.activities;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.bingo.bingo.R;
 
 public class SignInActivity extends AppCompatActivity {
 
-
+    EditText username;
+     EditText password;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_sign_in);}
-       // Button button = (Button) findViewById(R.id.button);
+        setContentView(R.layout.activity_sign_in);
 
-       /* button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                FindStoreFragment fragment = new FindStoreFragment();
-                FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
-                transaction.replace(R.id.flContent, fragment);
-                transaction.commit();
-            }
-        });*/
+        username=(EditText)findViewById(R.id.etUsername);
+        password=(EditText)findViewById(R.id.etPassword);
+
+    }
 
 
 
         public  void onSignIn(android.view.View view){
 
-            Intent intent = new Intent(this,MainActivity.class);
-            startActivity(intent);
+
+
+            if(username.getText().toString().equals("bingo") && password.getText().toString().equals("bingo")) {
+                               Toast.makeText(SignInActivity.this, "Login", Toast.LENGTH_LONG);
+                                Intent intent = new Intent(this, MainActivity.class);
+                                startActivity(intent);
+                                //Toast.makeText(LoginActivity.this,"Hello "+etUsername.getText().toString()+" .You logged in succesfully",Toast.LENGTH_LONG).show();
+                                    }
+            else {
+                    Toast.makeText(SignInActivity.this, "Invalid Username or Password", Toast.LENGTH_LONG);
+            }
+
             //Toast.makeText(LoginActivity.this,"Hello "+etUsername.getText().toString()+" .You logged in succesfully",Toast.LENGTH_LONG).show();
 
         }
